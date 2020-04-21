4b291249df368ccb6121171edfbcb7843e08d1ae
Victor Gushiken <sakaegushiken@gmail.com>
2020-04-21 19:54:32 -0300

    Add day0 solutions

---
+++ day0/helloWorld.js
@@ -0,0 +1,27 @@
+/**
+*   A line of code that prints "Hello, World!" on a new line is provided in the editor. 
+*   Write a second line of code that prints the contents of 'parameterVariable' on a new line.
+*
+*	Parameter:
+*   parameterVariable - A string of text.
+**/
+function greeting(parameterVariable) {
+    // This line prints 'Hello, World!' to the console:
+    console.log('Hello, World!');
+    console.log(parameterVariable)
+    // Write a line of code that prints parameterVariable to stdout using console.log:
+    
+}
+
+
+/**
+	Sample Input 0
+
+	Welcome to 10 Days of JavaScript!
+
+Sample Output 0
+
+Hello, World!
+Welcome to 10 Days of JavaScript!
+
+**/

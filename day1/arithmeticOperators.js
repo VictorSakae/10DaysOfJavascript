2d734f9453eda2b5063fbff470a2f329f0da4f95
Victor Gushiken <sakaegushiken@gmail.com>
2020-04-21 22:20:17 -0300

    Add day1 solutions

---
+++ day1/arithmeticOperators.js
@@ -0,0 +1,43 @@
+/**
+*   Calculate the area of a rectangle.
+*
+*   length: The length of the rectangle.
+*   width: The width of the rectangle.
+*   
+*	Return a number denoting the rectangle's area.
+**/
+function getArea(length, width) {
+    let area;
+    // Write your code here
+    area = length * width;
+    return area;
+}
+
+/**
+*   Calculate the perimeter of a rectangle.
+*	
+*	length: The length of the rectangle.
+*   width: The width of the rectangle.
+*   
+*	Return a number denoting the perimeter of a rectangle.
+**/
+function getPerimeter(length, width) {
+    let perimeter;
+    // Write your code here
+    perimeter = (2 * length) + (2 * width);
+    return perimeter;
+}
+
+/**
+Sample Input 0
+
+3
+4.5
+
+Sample Output 0
+
+13.5
+15
+
+
+**/
---
+++ day1/functions.js
@@ -0,0 +1,17 @@
+/*
+ * Create the function factorial here
+ */
+function factorial(n) {
+    return (n == 1) ? 1 : (n * factorial(n -1));
+}
+
+/**
+Sample Input 0
+
+4
+
+Sample Output 0
+
+24
+
+**/
